55374f5163e0e6951a3f5dd89173d602708a5223
Bradley Priest <bradleypriest@gmail.com>
2015-06-01 13:06:16 +0800

    Use ENV db var

---
+++ config/prod.secret.exs
@@ -0,0 +1,12 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+config :changeling, Changeling.Endpoint,
+  secret_key_base: {:system, "SECRET_KEY_BASE"}
+
+# Configure your database
+config :changeling, Changeling.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: {:system, "DATABASE_URL"}
