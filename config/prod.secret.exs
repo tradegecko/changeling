37166ff21043b62dd69ba771b01763ea5040d067
Bradley Priest <bradleypriest@gmail.com>
2015-06-01 13:28:53 +0800

    I don't knw0

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -4,7 +4,7 @@ use Mix.Config
 # you likely want to automate and keep it away from
 # your version control system.
 config :changeling, Changeling.Endpoint,
-  secret_key_base: {:system, "SECRET_KEY_BASE"}
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
 config :changeling, Changeling.Repo,
